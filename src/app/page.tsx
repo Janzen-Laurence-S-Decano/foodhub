41de80dc50dfb122c8f463ec4da1c7c5232ce7ec
jnzndcn <janzen.decano@gmail.com>
2025-02-05 02:17:39 -0800

    linking database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,13 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxTr0hnOpD2z8CsRHSevUM5cDrJIL961xbFyVd",
-  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxy50EPiICJ3S6fgOmXDqNnjHbEFe2ydr1PsuA",
-  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxu7gIq8xMNWyXbox6ds7D0cmHRKLYIFJvuUhO"
-
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-    id: index +1,
-    url,
-
-}));
 
 export default async function HomePage() {
   
 
-    const posts = await db.query.posts.findMany();
+    const images = await db.query.images.findMany({
+      orderBy: (model,{ desc }) => desc(model.id),
+    });
 
   return (
     <main className="">
@@ -30,13 +20,11 @@ export default async function HomePage() {
           </div>
         ))} */}
 
-            {posts.map((post) => (
-              <div key={post.id}>{post.name}</div>
-            ))}
 
-          {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          {images.map((image) => (
             <div key={image.id} className="w-48">
-              <img src={image.url}/>
+              <img src={image.url} />
+              <div>{image.name}</div>
             </div>
           ))}
 
