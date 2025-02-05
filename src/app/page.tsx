b8a7b51b9d85e2a450ce2ea965bec9aa64cd0b1d
jnzndcn <janzen.decano@gmail.com>
2025-02-04 23:02:09 -0800

    scaffold UI with Mock Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxTr0hnOpD2z8CsRHSevUM5cDrJIL961xbFyVd",
+  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxy50EPiICJ3S6fgOmXDqNnjHbEFe2ydr1PsuA",
+  "https://99mvyxc675.ufs.sh/f/3T9DhQRBd4jxu7gIq8xMNWyXbox6ds7D0cmHRKLYIFJvuUhO"
+
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+    id: index +1,
+    url,
+
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div> Foodhub(In Progress)</div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {/*{mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))} */}
+
+          {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url}/>
+            </div>
+          ))}
+
+      </div>
     </main>
   );
 }
